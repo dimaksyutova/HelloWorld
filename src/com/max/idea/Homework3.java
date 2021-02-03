13666311eb101c3e83757734c9b2714e6fe8bcd6
dmaksyutova <dilara.maksyutova@appline.ru>
2021-02-03 12:50:17 +0300

    Добавлены пробелы около математических операторов.

--- src/com/max/idea/Homework3.java
+++ src/com/max/idea/Homework3.java
@@ -6,12 +6,12 @@ public class Homework3 {
 
         //Меняем местами первый и последний элементы массива
         int temp = myArray[0];
-        myArray[0] = myArray[myArray.length-1];
-        myArray[myArray.length-1] = temp;
+        myArray[0] = myArray[myArray.length - 1];
+        myArray[myArray.length - 1] = temp;
         //Стал массив {5, 22, 3, 44, 1}
 
         //Выводим в консоль сумму первого и среднего элемента
-        int sum = myArray[0] + myArray[(myArray.length-1)/2];
+        int sum = myArray[0] + myArray[(myArray.length - 1) / 2];
         System.out.println("Сумма первого и среднего элементов: " + sum);
     }
 }
